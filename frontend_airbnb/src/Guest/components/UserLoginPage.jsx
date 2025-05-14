4a758b62f6b391d0b907d9f12cc9cb05a35d1386
LunarByteFlow <141225732+LunarByteFlow@users.noreply.github.com>
2025-05-14 20:25:32 +0500

    Update UserLoginPage.jsx

--- frontend_airbnb/src/Guest/components/UserLoginPage.jsx
+++ frontend_airbnb/src/Guest/components/UserLoginPage.jsx
@@ -82,7 +82,7 @@ const UserLoginPage=()=>{
     e.preventDefault();
 
     try {
-      const { data } = await axios.post("http://localhost:8000/api/login", credentials);
+      const { data } = await axios.post("https://c1cb-86-22-227-192.ngrok-free.app/api/login", credentials);
 
       // 1️⃣ store token (cookie or localStorage):
       Cookies.set("authToken", data.authToken, { sameSite: "lax" });
